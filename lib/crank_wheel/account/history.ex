3604b7131e843d2b644f9fc00451d82b89eb748d
Kleber Correia <klebervirgilio@gmail.com>
2019-03-12 09:33:15 +0000

    Fetch account history

--- lib/crank_wheel/account/history.ex
+++ lib/crank_wheel/account/history.ex
@@ -2,7 +2,7 @@ defmodule CrankWheel.Account.History do
   import CrankWheel
   alias CrankWheel.Client
 
-  @path "/ss/api/demo_usage"
+  @path "/ss/api/usage"
 
   def usage(client) do
     get(@path, client)
